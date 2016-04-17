91bca8b871865a4c99e6261a9737d2d93d04708a
root <root@monitor.konsom.ru>
2016-04-17 18:51:11 +0500

    removed run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --name opennms-database -e POSTGRES_PASSWORD=mAAAster -d postgres
-docker run -it --link=opennms-database:db -e DB_POSTGRES_PASSWORD=mAAAster --privileged --name opennms-17 stakly/opennms
